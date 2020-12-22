3c5b0cd1da6b8a5250128b64e351585a7913c184
Nnamani Ugochukwu F <royal.ugoh@gmail.com>
2020-12-22 18:49:59 +0100

    Add level 5 security using passport

--- app.js
+++ app.js
@@ -2,26 +2,43 @@ require( 'dotenv' ).config();
 const express = require( 'express' );
 const mongoose = require( 'mongoose' );
 const ejs = require( 'ejs' );
-const md5 = require( 'md5' );
-const bcrypt = require( 'bcrypt' );
-
-
+const session = require( 'express-session' );
+const passport = require( 'passport' );
+const passportLocalMongoose = require( 'passport-local-mongoose' );
 
 const app = express();
-const saltRounds = 10;
+
 
 app.set( 'view engine', 'ejs' );
 app.use( express.static( 'public' ) );
 app.use( express.urlencoded( { extended: true } ) );
+app.use( session( {
+	secret: 'Our little secret.',
+	resave: false,
+	saveUninitialized: false
+} ) )
+app.use(passport.initialize())
+app.use( passport.session() );
 
-mongoose.connect( 'mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true} );
+
+
+
+mongoose.connect( 'mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true } );
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema( {
 	email: String,
 	password: String
 } )
 
+userSchema.plugin( passportLocalMongoose );
+
 const User = mongoose.model( 'User', userSchema );
+
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get( '/', ( req, res ) => {
 	res.render( 'home' );
@@ -35,42 +52,48 @@ app.get( '/register', ( req, res ) => {
 	res.render( 'register' );
 } )
 
+app.get( '/secrets', ( req, res ) => {
+	if ( req.isAuthenticated() ) {
+		res.render( 'secrets' );
+	} else {
+		res.redirect( '/login' );
+	}
+})
+
+app.get( '/logout', function ( req, res ) {
+	req.logout()
+	res.redirect( '/' );
+})
+
 app.post( '/register', ( req, res ) => {
 	const { username, password } = req.body;
-	bcrypt.hash(password, saltRounds, function(err, hash) {
-	// Store hash in your password DB.
-		if ( !err ) {
-			const newUser = new User( {
-		email: username,
-			password: hash
-	} )
-	
-	newUser.save( ( err ) => {
+	User.register( { username: username }, password, function ( err, user ) {
 		if ( err ) {
-			console.log(err);
+			console.log( err );
+			res.redirect( '/register' );
 		} else {
-			res.render( 'secrets' );
+			passport.authenticate( 'local' )( req, res, function () {
+				res.redirect( '/secrets' );
+			})
 		}
 	})
-	}
-});
-
-	
 } )
 
 app.post( '/login', ( req, res ) => {
 	const { username, password } = req.body;
 
-	User.findOne( { email: username }, ( err, foundUser ) => {
+	const user = new User( {
+		username: username,
+		password: password
+	})
+
+	req.login( user, function ( err ) {
 		if ( err ) {
-			console.log(err);
+			console.log( err );
 		} else {
-			bcrypt.compare( password, foundUser.password, function ( err, result ) {
-				if ( result ) {
-				res.render( 'secrets' );
-			}
-    // result == true
-});
+			passport.authenticate('local')( req, res, function () {
+				res.redirect( '/secrets' );
+			})
 		}
 	})
 })
